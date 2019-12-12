227ec5662db1435b2ba91c7c80293c4803c72578
ilyxans <kovalyov21@gmail.com>
2019-12-12 23:00:44 +0200

    my first try to do task

--- script.js
+++ script.js
@@ -1,7 +1,55 @@
-let money = prompt("Your mounthly budjet?", "How mutch");
-let time = prompt("Enter date and time", "YYYY-MM-DD");
+'use strict';
 
-let expenses: {
-    "money",
-    "time"
-};
+// let persone = {
+//     name: "Jone",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(persone["name"]);
+
+// let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+// console.log(arr[0]);
+
+// alert("Hello World");
+
+// confirm("Are you 18 years old?");
+
+// let answer = confirm("Are you 18 years old?");
+
+// console.log(answer);
+
+// let answer = prompt("Are you 18?", "Yes");
+
+// console.log(answer);
+
+// let incr = 10,
+//     decr = 10;
+
+// ++incr;
+// --decr;
+
+// console.log(incr, decr);
+
+// console.log(5%2);
+// console.log("2" === 2);
+
+// let isChecked = true,
+//     isClose = false;
+
+// console.log(isChecked && isClose);
+
+let budget = +prompt("Your mounthly budjet", "");
+let date = prompt("Enter date and time", "YYYY-MM-DD");
+
+console.log(budget, date);
+
+let appData = [budget, date, expenses{
+    first: 'second'
+}];
+
+
+
+
+
